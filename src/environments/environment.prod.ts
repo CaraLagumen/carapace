c112105b38b01ccd0253acc64be8a20bc699332c
Cara Lagumen <45896071+CaraLagumen@users.noreply.github.com>
2020-04-04 23:39:34 -0700

    Delete environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: "https://cara-pace.firebaseio.com/",
-  apiKey: "AIzaSyBLsPFWir1y0whs9k51RiE_YEsNwWuOMD4",
-  bloggerUrl: "https://www.googleapis.com/blogger/v3/blogs/1524245072347705494",
-};
